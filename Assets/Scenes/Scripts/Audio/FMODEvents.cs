cbf35453a7e68724c9c2a54262bf9074c97749bd
fluorine1001 <fluorine1001@sasa.hs.kr>
2025-11-06 17:00:19 +0900

    FMOD 적용 및 총 발사 sfx 추가
    총에서 소리가 난다고!

---
+++ Assets/Scenes/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Bullet Launch SFX")]
+    [field: SerializeField] public EventReference BulletLaunched {get; private set;}
+
+    public static FMODEvents instance {get; private set;}
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("Found more than one FMOD Events instance in the scene.");
+        }
+        instance = this;
+    }
+}
